31149c096ece85915d1ac86998357242e4dc11ed
Skytlz <skytlzdev@gmail.com>
2025-01-22 18:15:10 -0500

    Inorder, Preorder, Postorder traversal

--- bst.cpp
+++ bst.cpp
@@ -2,6 +2,7 @@
 // Created by skytl on 1/21/25.
 //
 #include "bst.hpp"
+#include <iostream>
 
 Node *BST::insert(Person person) {
 
@@ -24,15 +25,27 @@ int BST::height(Node *node) {
 }
 
 void BST::inorder(Node *node) {
-
+    if  (node) {
+        inorder(node->left);
+        std::cout << node->person << std::endl;
+        inorder(node->right);
+    }
 }
 
 void BST::preorder(Node *node) {
-
+    if  (node) {
+        std::cout << node->person << std::endl;
+        preorder(node->left);
+        preorder(node->right);
+    }
 }
 
 void BST::postorder(Node *node) {
-
+    if  (node) {
+        postorder(node->left);
+        postorder(node->right);
+        std::cout << node->person << std::endl;
+    }
 }
 
 
